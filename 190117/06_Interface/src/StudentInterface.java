3327ea375fea9928dff8061992b2693dabc6ffef
= <taeho@example.com>
2019-01-17 17:08:27 +0900

    no message

--- 190117/06_Interface/src/StudentInterface.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface StudentInterface {
-	
-	public boolean login(String id, String pw); 
-	public String getInfo() ;
-	public void setPW(String pw);
-	
-}
--- 190117/06_Interface/src/com/ssafy/school/Student.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.school;
-
-public class Student {
-	
-}
